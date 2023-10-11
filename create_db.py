bcc5825546554677cf3b77ebc5556316cee2e457
Taras Semenchenko <semtaras20@gmail.com>
2023-10-11 21:06:09 +0300

    add script for creating database

---
+++ create_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect("database/database.db")
+
+cursor = conn.cursor()
+sql_script_paths = [
+    "database\\scripts\\Cardio.sql",
+    "database\\scripts\\HighCarb.sql",
+    "database\\scripts\\LowCarb.sql",
+    "database\\scripts\\ModerateCarb.sql",
+    "database\\scripts\\StandardCalories.sql",
+]
+
+for sql_script_path in sql_script_paths:
+    with open(sql_script_path, "r") as file:
+        sql_script = file.read()
+    cursor.executescript(sql_script)
+
+conn.close()
